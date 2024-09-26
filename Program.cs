a4896fdee2f881889834815b32faef2e8404c419
ankit-dhaka <dankit@ariqt.com>
2024-09-26 17:18:47 +0530

    hehe

--- Program.cs
+++ Program.cs
@@ -3,4 +3,8 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("change 1");
 Console.WriteLine("Change 2 direct push");
 Console.WriteLine("Change 3 cmd");
+Console.WriteLine("Exploring git diff");
+Console.WriteLine("Exploring git diff");
+
+Console.WriteLine("Exploring git diff");
 Console.WriteLine("change 4");
